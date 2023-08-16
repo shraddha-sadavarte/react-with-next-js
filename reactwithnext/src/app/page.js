9bcb401235aa8680f1cbb2074dc7119a95dbda8a
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2023-08-16 15:37:37 -0700

    get images using axios

--- reactwithnext/src/app/page.js
+++ reactwithnext/src/app/page.js
@@ -1,10 +1,36 @@
 "use client"
-import React from 'react' 
+import React, { useState } from 'react' ;
+import './globals.css';
+import axios from 'axios';
 
 const page = () => {
+
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+   try {
+    const response = await axios.get("https://picsum.photos/v2/list");
+    const data = response.data;
+    setImages(data);
+    console.log(data)
+
+   } catch (error) {
+    console.error("Error in Fetching Images");
+   }
+  }
+
   return (
+    
     <>
       <h1>This is home page</h1>
+      <button onClick={getImages} className='get'>Get Images</button>
+      <div className='getImage'>
+        {
+          images.map((elem, i) => {
+            return <img src={elem.download_url} key={i} height="200px" width="200px" className='image'/>
+          })
+        }
+      </div>
     </>
   )
 }
